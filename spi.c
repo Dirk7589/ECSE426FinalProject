ff48d98a80f5c9027ee78f3a00066bfa11015848
unknown <aslak@156TR4160-16.campus.MCGILL.CA>
2013-04-07 16:09:33 -0400

    Adjusted the chip select line

--- spi.c
+++ spi.c
@@ -46,7 +46,7 @@ void initSPI(void){
   GPIO_Init(GPIOA, &GPIO_InitStructure);
 	
 	/* Configure GPIO PIN for Wireless Chip select */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
