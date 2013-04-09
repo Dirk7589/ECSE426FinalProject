cf5c60483b127243809dc897eade5bd4bd61b8b8
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-04-09 09:17:09 -0400

    Initial Commit

---
+++ dac.c
@@ -0,0 +1,9 @@
+ /**
+*@file dac.c
+*@author Dirk Dubois, Alain Slak
+*@date April 9th, 2013
+*@brief A set of functions to communicate with the DAC
+*/
+
+#include "dac.h"
+#include "stm32f4_discovery_audio_codec.h"
---
+++ dac.h
@@ -0,0 +1,12 @@
+ /**
+*@file dac.h
+*@author Dirk Dubois, Alain Slak
+*@date April 9th, 2013
+*@brief A set of functions to communicate with the DAC
+*/
+
+#ifndef __DAC_H
+#define __DAC_H
+
+
+#endif
