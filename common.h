d508f223f6a1df5385895beeaf39ef181b3fb2a5
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-04-09 13:02:50 -0400

    Cleaned up file added all necessary variables

--- common.h
+++ common.h
@@ -11,26 +11,48 @@
 #include "cmsis_os.h"
 #include "wireless.h"
 
+/**Common Defines**/
+#define GREEN_LED 0x1000 /*!<Defines the bit location of the green LED*/
+#define ORANGE_LED 0x2000 /**< Defines the bit location of the orange LED*/
+#define RED_LED 0x4000 /**< Defines the bit location of the red LED*/
+#define BLUE_LED 0x8000	/**< Defines the bit location of the blue LED*/
+
 /*Global Variables*/
 extern float temperature; /**<The temperature variable*/
 extern float accCorrectedValues[3]; /**<The corrected accelerometer values*/
 extern float wirelessAccValues[3]; /**<Accelerometer values from the other board*/
 extern float angles[2]; /**<A variable containing the pitch and roll */
 extern int8_t wirelessAngles[2];
+extern uint8_t audioVolume; /**<The value used to determine the output audio volume*/
+extern char key;
+extern uint8_t LEDState; /**<A variable containing the current LED state*/
+extern uint8_t orientationMatch;
+extern uint8_t LEDCounter;
+
+/*********DMA Variables********/
 extern int8_t txWireless[WIRELESS_BUFFER_SIZE]; /**<Transmission buffer for Wireless for DMA*/
 extern int8_t rxWireless[WIRELESS_BUFFER_SIZE]; /**<Receive buffer for Wireless for DMA*/
-extern uint8_t strobeCommand[1];
-extern uint8_t status[1];
-extern uint8_t audioVolume; /**<The value used to determine the output audio volume*/
-
 extern uint8_t txWirelessInit[WIRELESS_BUFFER_INIT_SIZE]; /**<Transmission buffer for Wireless initialization for DMA*/
 extern uint8_t rxWirelessInit[WIRELESS_BUFFER_INIT_SIZE]; /**<Receive buffer for Wireless initialization for DMA*/
+extern uint8_t strobeCommand[1];
+extern uint8_t status[1];
 
+/**********Flags**************/
 extern uint8_t dmaFromAccFlag; /**<A flag variable that represents whether or not DMA was called from the accelerometer thread*/
 extern uint8_t dmaFromWirelessFlag; /**<A flag variable that represents whether or not DMA was called from the wireless thread*/
-extern uint8_t wirelessRdy;
-extern uint8_t dmaFlag;
-//extern uint8_t dmaInitFlag;
+
+
+/**********OS Signal Masks**********/
+extern uint8_t sampleACCFlag; /**<A flag variable for sampling, restricted to a value of 0 or 1*/
+extern uint8_t readKeypadFlag; /**<A flag variable that represents the keypad poll*/
+extern uint8_t displayLCDFlag; /**<A flag variable that represents the display poll*/
+extern uint8_t dmaFlag; /**<A flag variable that represents the dma poll*/
+
+/******Thread Prototypes*************/
+extern osThreadId aThread; //Accelerometer thread ID
+extern osThreadId wThread; //Wireless thread ID
+extern osThreadId kThread; //Keypad thread ID
+extern osThreadId lThread; //LCD thread ID
 
 extern osSemaphoreId wirelessAccId;
 extern osSemaphoreId accId; /**<The id for the accCorrectedValues semaphore*/
@@ -43,5 +65,9 @@ extern osMutexId dmaId; /**<The id for the DMA mutex*/
 extern osTimerId vlmUpId; /**<The id for the audio volume up EXTI timer*/
 extern osTimerId vlmDownId; /**<The id for the audio volume down EXTI timer*/
 
-extern char key;
+//ISR States and Flags
+extern uint8_t buttonState; /**<A variable that represents the current state of the button*/
+extern uint8_t tapState; /**<A variable that represents the current state of the tap detect*/
+extern uint8_t volumeUpBtn; /**<A variable that represents the state of the volume up button*/
+extern uint8_t volumeDownBtn; /**<A variable that represents the state of the volume down button*/
 #endif
