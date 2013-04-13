5306650d8275ff81025e670b7599ea76257e1d33
unknown <bgingr2@156TR4160-18.campus.MCGILL.CA>
2013-04-13 14:54:56 -0400

    Added init functionality

--- dac.c
+++ dac.c
@@ -20,45 +20,41 @@ void playInit(void){
 	EVAL_AUDIO_SetAudioInterface(AUDIO_INTERFACE_I2S);
 
 	EVAL_AUDIO_Init(OUTPUT_DEVICE_AUTO, 70, I2S_AudioFreq_22k);
-	/* Initialize the Audio codec and all related peripherals (I2S, I2C, IOExpander, IOs...) */  
-//	Codec_Init(OUTPUT_DEVICE_AUTO, VOLUME_CONVERT(70), I2S_AudioFreq_22k);
-	//Audio_MAL_Init();
-	
+
 	dacStruct.DMA_Channel = AUDIO_I2S_DMA_CHANNEL;  
-    dacStruct.DMA_PeripheralBaseAddr = AUDIO_I2S_DMA_DREG;
-    dacStruct.DMA_Memory0BaseAddr = (uint32_t)0;      /* This field will be configured in play function */
-    dacStruct.DMA_DIR = DMA_DIR_MemoryToPeripheral;
-    dacStruct.DMA_BufferSize = (uint32_t)0xFFFE;      /* This field will be configured in play function */
-    dacStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-    dacStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
-    dacStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-    dacStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord; 
-    dacStruct.DMA_Mode = DMA_Mode_Normal;
+	dacStruct.DMA_PeripheralBaseAddr = AUDIO_I2S_DMA_DREG;
+	dacStruct.DMA_Memory0BaseAddr = (uint32_t)0;      /* This field will be configured in play function */
+	dacStruct.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+	dacStruct.DMA_BufferSize = (uint32_t)0xFFFE;      /* This field will be configured in play function */
+	dacStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	dacStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	dacStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	dacStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord; 
+	dacStruct.DMA_Mode = DMA_Mode_Circular;
 	dacStruct.DMA_Priority = DMA_Priority_High;
-    dacStruct.DMA_FIFOMode = DMA_FIFOMode_Disable;         
-    dacStruct.DMA_FIFOThreshold = DMA_FIFOThreshold_1QuarterFull;
-    dacStruct.DMA_MemoryBurst = DMA_MemoryBurst_Single;
-    dacStruct.DMA_PeripheralBurst = DMA_PeripheralBurst_Single; 
-	
+	dacStruct.DMA_FIFOMode = DMA_FIFOMode_Disable;         
+	dacStruct.DMA_FIFOThreshold = DMA_FIFOThreshold_1QuarterFull;
+	dacStruct.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+	dacStruct.DMA_PeripheralBurst = DMA_PeripheralBurst_Single; 
+
+	DMA_Init(AUDIO_I2S_DMA_STREAM, &dacStruct);
 }
 
 void play(uint16_t* audioTone){
 	
-	dacStruct.DMA_Memory0BaseAddr = (uint32_t)audioTone;
-    dacStruct.DMA_BufferSize = (uint32_t)55125;
-	
-    
-    /* Configure the DMA Stream with the new parameters */
-    DMA_Init(AUDIO_I2S_DMA_STREAM, &dacStruct);
-    
-    /* Enable the I2S DMA Stream*/
-    DMA_Cmd(AUDIO_I2S_DMA_STREAM, ENABLE);
-
+	dacStruct.DMA_Memory0BaseAddr = (uint32_t)audioTone; //Select the tone to play
+	dacStruct.DMA_BufferSize = (uint32_t)BUFFER_SIZE; //Specify the buffer size
   /* If the I2S peripheral is still not enabled, enable it */
   if ((CODEC_I2S->I2SCFGR & 0x0400) == 0)
   {
     I2S_Cmd(CODEC_I2S, ENABLE);
   }
+	
+	/* Configure the DMA Stream with the new parameters */
+	DMA_Init(AUDIO_I2S_DMA_STREAM, &dacStruct); //Intialize it
+
+	/* Enable the I2S DMA Stream*/
+	DMA_Cmd(AUDIO_I2S_DMA_STREAM, ENABLE); //Turn on DMA
 }
 
 void adjustPitch(uint16_t* sinTable, uint16_t desiredFreq) {
